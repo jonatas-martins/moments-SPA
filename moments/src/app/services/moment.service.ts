a1b8e143cd7d7e0b5cb450688fa5d28c2213b834
Jonatas Martins <107153976+jonatas-martins@users.noreply.github.com>
2022-12-27 15:22:02 -0300

    feat: Rescuing data from the bank

--- moments/src/app/services/moment.service.ts
+++ moments/src/app/services/moment.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { Moment } from '../Moments';
+import { Response } from '../Response';
 
 import { environment } from 'src/environments/environment';
 
@@ -14,6 +15,10 @@ export class MomentService {
   private apiUrl = `${this.baseApiUrl}api/moments`;
 
   constructor(private http: HttpClient) {}
+
+  getMoments(): Observable<Response<Moment[]>> {
+    return this.http.get<Response<Moment[]>>(this.apiUrl);
+  }
 
   createMoment(formData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formData);
